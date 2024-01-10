812e09757f2aa9fbc2423e74cf10b46ffd6a970e
Noah Pittman <noahpittman00@gmail.com>
2024-01-09 19:08:15 -0500

    redirect to generate on load

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 const Home = () => {
-	return (
-		<div className="flex select-none flex-col space-y-4 justify-center items-center min-h-screen text-center">
-			<Button>
-				<Link prefetch href={"/generate"}>
-					Enter app
-				</Link>
-			</Button>
-		</div>
-	);
+	return redirect("/generate");
+	// <div className="flex select-none flex-col space-y-4 justify-center items-center min-h-screen text-center">
+	// 	<Button>
+	// 		<Link prefetch href={"/generate"}>
+	// 			Enter app
+	// 		</Link>
+	// 	</Button>
+	// </div>
 };
 
 export default Home;
